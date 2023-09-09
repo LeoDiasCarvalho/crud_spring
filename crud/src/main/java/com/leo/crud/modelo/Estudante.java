e581c895679fe9abd911f0a480bcb1eb6809b614
LeoDiasCarvalho <leodiascarvalhosr@gmail.com>
2023-09-09 18:45:41 -0300

    Criado a classe Estudante

---
+++ crud/src/main/java/com/leo/crud/modelo/Estudante.java
@@ -0,0 +1,38 @@
+package com.leo.crud.modelo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "estudante")
+public class Estudante {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@NotBlank(message = "O nome não pode ser vazio!")
+	@Size(min = 3, message = "O nome não pode conter menos que 3 caracteres!")
+	@Column(name = "nome")
+	private String nome;
+	
+	@NotBlank(message = "A idade não pode ser vazia!")
+	@Min(value = 18, message = "O estudante deve ter no mínimo 18 anos!")
+	@Column(name = "idade")
+	private int idade;
+
+}
