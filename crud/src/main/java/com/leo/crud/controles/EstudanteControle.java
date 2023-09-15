ba3fd86c14713b1985d10cab8fdb8cbc88ea1d28
LeoDiasCarvalho <leodiascarvalhosr@gmail.com>
2023-09-15 20:07:11 -0300

    Validando os dados do formulário usando Java Bean Validation

--- crud/src/main/java/com/leo/crud/controles/EstudanteControle.java
+++ crud/src/main/java/com/leo/crud/controles/EstudanteControle.java
@@ -3,6 +3,7 @@ package com.leo.crud.controles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.leo.crud.modelo.Estudante;
 import com.leo.crud.servicos.EstudanteServico;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class EstudanteControle {
@@ -30,7 +33,11 @@ public class EstudanteControle {
 	}
 	
 	@PostMapping("/gravar")
-	public String gravarEstudante(@ModelAttribute("novoEstudante") Estudante estudante, RedirectAttributes attributes) {
+	public String gravarEstudante(@ModelAttribute("novoEstudante") @Valid Estudante estudante,
+			BindingResult erros ,RedirectAttributes attributes) {
+		if(erros.hasErrors()) {
+			return "/novo-estudante";
+		}
 		estudanteServico.salvarEstudante(estudante);
 		attributes.addFlashAttribute("mensagem", "Estudante salvo com sucesso!");
 		return "redirect:/novo";
